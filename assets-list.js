7b28843aa277126f10417809c5369f03e50ab18b
amilcarpg <amilcarpg@hotmail.com>
2026-01-29 17:04:37 -0500

    mas asssets

--- assets-list.js
+++ assets-list.js
@@ -1,4 +1,4 @@
 const ASSETS = [
   { label: "Pony mágico", src: "assets/pony1.png" },
-  { label: "Dinosaurio", src: "assets/dinosaur-01.png" },
+  { label: "Pony Princesa", src: "assets/pony2.png" },
 ];
