b08125e27af8099d4fe8ac34b8869d530a865acc
amilcarpg <amilcarpg@hotmail.com>
2026-01-29 17:09:48 -0500

    lista de assets

--- assets-list.js
+++ assets-list.js
@@ -1,4 +1,24 @@
-const ASSETS = [
+/*
+ * Listado de imágenes PNG para colorear. Cada elemento tiene una etiqueta
+ * descriptiva y una ruta relativa a la carpeta "assets". Estas imágenes
+ * incluyen ponys, casas y paisajes en formato de dibujo lineal para que
+ * puedan ser coloreadas en una aplicación web o proyecto similar.
+ */
+
+export const ASSETS = [
   { label: "Pony mágico", src: "assets/pony1.png" },
   { label: "Pony Princesa", src: "assets/pony2.png" },
+  { label: "Pony 3", src: "assets/pony3.png" },
+  { label: "Pony 4", src: "assets/pony4.png" },
+
+  { label: "Casa 1", src: "assets/house1.png" },
+  { label: "Casa 2", src: "assets/house2.png" },
+  { label: "Casa 3", src: "assets/house3.png" },
+  { label: "Casa 4", src: "assets/house4.png" },
+  { label: "Casa 5", src: "assets/house5.png" },
+  { label: "Paisaje 1", src: "assets/landscape1.png" },
+  { label: "Paisaje 2", src: "assets/landscape2.png" },
+  { label: "Paisaje 3", src: "assets/landscape3.png" },
+  { label: "Paisaje 4", src: "assets/landscape4.png" },
+  { label: "Paisaje 5", src: "assets/landscape5.png" }
 ];
